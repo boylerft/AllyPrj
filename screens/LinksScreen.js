96870fca94e7dfdb2218876770d7714944f5a3cb
boylerft <31903023+boylerft@users.noreply.github.com>
2019-01-25 01:01:47 -0800

    Added map page

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Map',
-  };
-
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+        <MapView style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+            }}
+        >
+        </MapView>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+ container: {
+   position: 'absolute',
+   top: 0,
+   left: 0,
+   bottom: 0,
+   right: 0,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+  map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0
+  }
+
 });
